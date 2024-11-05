13cca38cd7eb59d7bb2618b24c509a03002f7e3a
TuTiDore <tutidore@gmail.com>
2024-11-04 22:48:37 -0500

    extract switch case

--- index.js
+++ index.js
@@ -19,42 +19,34 @@ function createSVGRect(rectProps) {
 
 function generate() {
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    document.body.appendChild(svg);
     svg.setAttribute("height", "100%");
     svg.setAttribute("width", "100%");
     
     for (let i = 0; i < 88; i++) {
-        let x = 24 * Math.round(7 / 12 * i);
-        switch (i % 12) {
-            case 0:
-                svg.appendChild(createWhiteKey(x, 0));
-                if (i > 0) {
-                    svg.appendChild(createBlackKey(x - 7, 0));
-                }
-                break;
-            case 2:
-                svg.appendChild(createWhiteKey(x, 0));
-                svg.appendChild(createBlackKey(x - 5, 0));
-                break;
-            case 3:
-                svg.appendChild(createWhiteKey(x, 0));
-                break;
-            case 5:
-                svg.appendChild(createWhiteKey(x, 0));
-                svg.appendChild(createBlackKey(x - 9, 0));
-                break;
-            case 7:
-                svg.appendChild(createWhiteKey(x, 0));
-                svg.appendChild(createBlackKey(x - 5, 0));
-                break;
-            case 8:
-                svg.appendChild(createWhiteKey(x, 0));
-                break;
-            case 10:
-                svg.appendChild(createWhiteKey(x, 0));
-                svg.appendChild(createBlackKey(x - 9, 0));
-                break;
-        }
+        for (let newKey of keysAtPosition(i))
+            svg.appendChild(newKey);
     }
+}
 
-    document.body.appendChild(svg);
+function keysAtPosition(i) {
+    const x = 24 * Math.round(7 / 12 * i);
+    switch (i % 12) {
+        case 0:
+            return [createWhiteKey(x, 0), ...(i > 0 ? [createBlackKey(x - 7, 0)] : [])];
+        case 2:
+            return [createWhiteKey(x, 0), createBlackKey(x - 5, 0)];
+        case 3:
+            return [createWhiteKey(x, 0)];
+        case 5:
+            return [createWhiteKey(x, 0), createBlackKey(x - 9, 0)];
+        case 7:
+            return [createWhiteKey(x, 0), createBlackKey(x - 5, 0)];
+        case 8:
+            return [createWhiteKey(x, 0)];
+        case 10:
+            return [createWhiteKey(x, 0), createBlackKey(x - 9, 0)];
+        default:
+            return []
+    }
 }
